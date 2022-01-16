d00839c5f382986e4243819bbacc42b0fed89713
Leandro de Brito Alencar <zzleandrobritozz@gmail.com>
2022-01-15 23:18:01 -0300

    fix updateUser

--- src/graphql/mutations/updateUser.js
+++ src/graphql/mutations/updateUser.js
@@ -1,10 +1,11 @@
-const { GraphQLObjectType, GraphQLString } = require('graphql');
+const { GraphQLString } = require('graphql');
 const userGraphQLType = require('../types/userType');
 const User = require('../../models/userModel');
 
 module.exports = {
   type: userGraphQLType,
   args: {
+    id: { type: GraphQLString},
     name: { type: GraphQLString },
     username: { type: GraphQLString },
     email: { type: GraphQLString },
@@ -15,7 +16,7 @@ module.exports = {
       const user = await User.findById(args.id);
       user.name = args.name;
       user.username = args.username,
-        user.email = args.email;
+      user.email = args.email;
       const updatedUser = user.save();
       return updatedUser;
     } catch (err) {
